c2d0fd963aff4fb8315348280820321047818e63
HexHyperion <szymonjan@gmail.com>
2025-06-14 20:13:27 +0200

    Make title screen animated

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,46 @@
-import RectangularButton from "@/components/RectangularButton";
+import CircularButton from "@/components/CircularButton";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useRouter } from "expo-router";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { useEffect, useRef } from "react";
+import { Animated, Dimensions, Easing, StatusBar, StyleSheet, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+
+  const rotateAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(rotateAnim, {
+        toValue: 1,
+        duration: 10000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    ).start();
+  }, [rotateAnim]);
+
+  const rotate = rotateAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
 
   return (
     <>
     <StatusBar barStyle="light-content"/>
     <View style={styles.body}>
-      <Text style={styles.header}>e kamer</Text>
-      <View>
-        <Text style={styles.text}>Your absolute best camera app out there.</Text>
-        <Text style={styles.text}>Ready for this awesome adventure?</Text>
+      <View style={styles.iconContainer}>
+        <Animated.View style={{ transform: [{ rotate }] }}>
+          <MaterialIcons name="camera" size={screenWidth*2} color="#0f0f0f" />
+        </Animated.View>
       </View>
-      <RectangularButton title="yes!" onPress={() => router.navigate("/gallery")}/>
+      <CircularButton title="kamera sztart" size={100} onPress={() => router.navigate("/gallery")}/>
     </View>
     </>
   );
 }
+
+const screenWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   body: {
@@ -27,6 +49,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     gap: 10
+  },
+  iconContainer: {
+    width: screenWidth * 2,
+    height: screenWidth * 2,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -screenWidth }, { translateY: -screenWidth }],
+    zIndex: -1,
   },
   header: {
     fontFamily: "monospace",
